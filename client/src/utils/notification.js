e1d998300c12c203babebdb429f471a621a87726
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-05-24 03:05:08 +0530

    fixed very minute typo error

--- client/src/utils/notification.js
+++ client/src/utils/notification.js
@@ -2,12 +2,12 @@ import {toast} from "react-toastify"
 
 export const successmessage=(message)=>{
     toast.success(message,{
-        postion:top-right
+        postion:"top-right"
     })
 }
 
 export const errormessage=(message)=>{
     toast.error(message,{
-        postion:top-right
+        postion:"top-right"
     })
 }
