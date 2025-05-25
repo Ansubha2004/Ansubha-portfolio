21899e63f0d70fb18c37c2ef268f637a3908aece
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-05-25 22:51:14 +0530

    backend deploy

--- server/server.js
+++ server/server.js
@@ -1,57 +1,42 @@
-import express from "express"
-import cors from "cors"
-import "./models/config.js"
-import formroute from "./routes/formroute.js"
+import express from "express";
+import cors from "cors";
+import "./models/config.js";
+import formroute from "./routes/formroute.js";
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
 
-
-const app=express();
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
-))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin:[
+        'http://localhost:5173', // for local dev
+        'https://ansubha-portfolio-jqh9.vercel.app/' // for production frontend
+      ],
+      
+    credentials: true,
+  })
+);
 
 //setting test api
-app.get('/formapi/test',(req,res)=>{
-    try{
-        return res.status(200).json({
-            success:true,
-            message:"API WORKING. Test successful"
-        })
-    }
-    catch(err)
-    {   
-        return res.status(500).json({
-            success:false,
-            message:err
-        })
-    }
-})
+app.get("/", (req, res) => {
+  res.send("Backend is Working");
+});
 
-app.use("/formapi",formroute);
-
-
-
-
-
+app.use("/formapi", formroute);
 
 //error route handling
-app.all('*',(req,res)=>{
-    return res.status(404).json({
-        success:false,
-        message:"Route error"
-    })
-})
-
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    success: false,
+    message: "Route error",
+  });
+});
 
 //setting up the server
-const port=process.env.PORT;
-app.listen(port,()=>{
-    console.log(`Running on port ${port}..........`)
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Running on port ${port}..........`);
+});
